15518725bd07c91e91cddda2fbbdb09e6934fe30
Ali Dabirmoghaddam <alid@craigsdance.cse.ucsc.edu>
2012-10-11 13:07:28 -0700

    Fixed some naming errors.

--- ali/lasso_runner.m
+++ ali/lasso_runner.m
@@ -1,7 +1,7 @@
-load('census.mat')
+load('census_v4.mat')
 header = header{1};
-mail_return_rate = data(:,169);
-data(:,169) = [];
+mail_return_rate = training_data(:,169);
+training_data(:,169) = [];
 any(~isnan(training_data(:,6)))
 training_data(:,6) = [];
 
